ebcdf75c9e6c9dfeb8497b573edafa9052f06097
Engr sher <sherdawar210945@gmail.com>
2023-10-12 17:29:14 +0400

    adding redux tool kit

--- api/routes/auth-route.js
+++ api/routes/auth-route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { signin, userAuth } from '../controllers/auth-controller.js';
+import { signin, signup} from '../controllers/auth-controller.js';
 
 const router = express.Router();
 
-router.post('/signup', userAuth);
+router.post('/signup', signup);
 router.post('/signin', signin)
 
 export default router
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInFailure, signInStart, signInSuccess } from "../redux/user/userSlice";
 
-export default function SignIn() {
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const {loading, error} = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart())
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message))
+        return;
+      }
+      dispatch(signInSuccess(data))
+      navigate("/");
+    } catch (error) {
+     dispatch(signInFailure(error.message))
+    }
+  };
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl p-3 text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          className="border p-3 rounded-lg"
+          type="text"
+          placeholder="email"
+          id="email"
+          onChange={handleChange}
+        ></input>
+        <input
+          className="border p-3 rounded-lg"
+          type="text"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+        ></input>
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80 uppercase"
+        >
+          {loading ? "loading...." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex m-3 gap-2">
+        <p>Dont have an account?</p>
+        <Link to="/sign-up" className="text-sky-600">
+          Sign Up
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
