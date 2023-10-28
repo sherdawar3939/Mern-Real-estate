f5834aa0ac165c62bd9f840c6a265f3540bc4854
Engr sher <sherdawar210945@gmail.com>
2023-10-28 16:35:19 +0400

    create search api route

--- api/routes/user-route.js
+++ api/routes/user-route.js
@@ -1,11 +1,12 @@
 import Express from "express";
-import { deleteUser, getUserListings, updateUser } from "../controllers/user-controller.js";
+import { deleteUser, getUser, getUserListings, updateUser } from "../controllers/user-controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = Express.Router();
 
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/listing/:id", verifyToken, getUserListings)
+router.get("/listing/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
